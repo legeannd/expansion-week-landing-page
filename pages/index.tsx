0b5beeb266b65505f54374446fcb90b49eb7f94e
Gean Lucas <gean.lfpsa@gmail.com>
2020-11-19 22:56:55 -0300

    Added landing page text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,49 @@
-import { Container, Footer, Header, HeaderDetails, HeaderImg, Title } from "../styles";
+import { Container, Footer, Header, HeaderDetails, HeaderImg, Main, MainSubtitle, MainText, Title } from "../styles";
 import Image from "next/image";
+import React from "react";
 
 const Index = () => {
   return (
     <Container>
       <Header>
         <Title>Você é antifrágil?</Title>
-        <HeaderDetails>Saiba AGORA por qual motivo essa é uma das <span>soft skills mais importantes</span> na carreira de um dev!</HeaderDetails>
+        <HeaderDetails>Saiba AGORA por qual motivo essa é uma das <strong>soft skills mais importantes</strong> na carreira de um dev!</HeaderDetails>
         <HeaderImg src="/assets/banner.jpg" alt="Frase motivacional"/>
       </Header>
+      
+      <Main>
+        <MainSubtitle>Antifragilidade é transceder o comportamento de evitar riscos</MainSubtitle>
+        <MainText>
+          Muita gente conhece o conceito de resiliência, que consiste basicamente na capacidade de passar pelas mais variadas adversidades sem que isso lhe abale.
+        </MainText>
+        <MainText>
+          No entanto, talvez mais importante do que conseguir voltar ao estado anterior após uma adversidade, é conseguir usar essa pedra no caminho como um <strong>impulso para evoluir.</strong>
+        </MainText>
+        <MainSubtitle>Uma mente que se expande para uma nova ideia, nunca mais volta ao seu tamanho original</MainSubtitle>
+        <HeaderImg src="/assets/main-1.jpg" alt="Fluxo de trabalho" width="50%"/>
+        <MainText>
+          É muito comum que durante a nossa carreira, encontremos toda sorte de desafios e problemas, e o nosso trabalho consiste em passar por eles.
+        </MainText>
+        <MainText>
+          Uma pessoa antifrágil consegue correr o risco indo em novas experiências e ainda pegar ensinamentos nessas situações, o que é muito importante para que a sua <strong>evolução seja contínua.</strong> 
+        </MainText>
+        <MainText>
+          Veja, se você vai a uma entrevista de emprego e recebe um não, não é motivo para ficar revoltado e desistir de tentar. Muito pelo contrário,
+          você pode aproveitar essa oportunidade para entender quais as habilidades que você ainda não domina, e se preparar para as próximas entrevistas.
+        </MainText>
+        <MainSubtitle>
+          Você não escolhe o que vai te atingir, mas escolhe como vai reagir a isso
+        </MainSubtitle>
+        <MainText>
+          Sendo antifrágil, você se torna <strong>capaz de lidar</strong> com os problemas e desafios que aparecem, e sair cada vez mais forte deles. Com isso, 
+          fica cada vez mais difícil se encontrar em uma situação ruim, de difícil resolução, pois você sabe que a unica opção é seguir em frente, 
+          passar por isso e <strong>aprender</strong> o que deve ser feito da próxima vez.
+        </MainText>
+      </Main>
 
       <Footer>
-        <span>Photo by <a href="https://unsplash.com/@theblowup?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">the blowup</a> on <a href="https://unsplash.com/t/business-work?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+        <span>Photo 1 by <a href="https://unsplash.com/@theblowup?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">the blowup</a> on <a href="https://unsplash.com/t/business-work?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+        <span>Photo 2 by <a href="https://unsplash.com/@sigmund?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Sigmund</a> on <a href="https://unsplash.com/t/business-work?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
       </Footer>
     </Container>
   )
--- styles/index.ts
+++ styles/index.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div`
   height: 100vh;
@@ -9,14 +10,11 @@ export const Container = styled.div`
 `;
 
 export const Header = styled.header`
-  margin: 0 120px;
+  margin: 0 120px 50px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  
-  @media(max-width: 1000px) {
-    margin: 0;
-  }
+
 `;
 
 export const Title = styled.h1`
@@ -29,26 +27,64 @@ export const HeaderDetails = styled.p`
   color: white;
   font-size: 24px;
   width: 100%;
-
-  > span {
-    font-weight: bold;
-    text-align: center;
-  }
+  margin-bottom: 50px;
 `;
 
 export const HeaderImg = styled.img`
-  max-width: 80%;
+  max-width: 70%;
+  height: auto;
+
+  @media(max-width: 1000px) {
+    max-width: 100%;
+    margin-top: 20px;
+  }
 `;
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  display: flex;
+  margin: 0 120px;
+  flex-direction: column;
+  align-items: center;
+
+  @media(max-width: 1000px) {
+    margin: 0;
+  }
+`;
+
+export const MainSubtitle = styled.h2`
+  color: white;
+  font-size: 30px;
+  text-align: center;
+
+  @media(max-width: 1000px){
+    font-size: 26px;
+    margin: 0 20px;
+  }
+`;
+
+export const MainText = styled.p`
+  width: 60%;
+  font-size: 24px;
+  color: ${shade(0.1, 'white')};
+
+  @media(max-width: 1000px) {
+    width: 80%;
+    font-size: 22px;
+  }
+
+`;
 
 export const Footer = styled.footer`
-  background: rgba(24, 25, 37, 100);
+  background: ${shade(0.7, '#181925')};
   width: 100%;
+  margin-top: 100px;
   color: grey;
   padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
 
   > span {
+    width: 100%;
     > a:hover {
       color: blue;
     }
