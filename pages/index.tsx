d73e56a6d5927753c5dbfdc7330df60aceeb57b9
Gean Lucas <gean.lfpsa@gmail.com>
2020-11-19 21:50:31 -0300

    Added image to header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
-import { Header } from "../styles";
+import { Container, Footer, Header, HeaderDetails, HeaderImg, Title } from "../styles";
+import Image from "next/image";
 
 const Index = () => {
   return (
-    <Header>teste</Header>
+    <Container>
+      <Header>
+        <Title>Você é antifrágil?</Title>
+        <HeaderDetails>Saiba AGORA por qual motivo essa é uma das <span>soft skills mais importantes</span> na carreira de um dev!</HeaderDetails>
+        <HeaderImg src="/assets/banner.jpg" alt="Frase motivacional"/>
+      </Header>
+
+      <Footer>
+        <span>Photo by <a href="https://unsplash.com/@theblowup?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">the blowup</a> on <a href="https://unsplash.com/t/business-work?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+      </Footer>
+    </Container>
   )
 }
 
--- styles/index.ts
+++ styles/index.ts
@@ -1,15 +1,56 @@
 import styled from 'styled-components';
 
-export const Header = styled.header`
-  background: purple;
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-  @media (min-width: 1120px) {
-    margin: 0 120px;
+export const Header = styled.header`
+  margin: 0 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  @media(max-width: 1000px) {
+    margin: 0;
   }
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  color: white;
+  font-size: 48px;
+  text-align: center;
+`;
+
+export const HeaderDetails = styled.p`
+  color: white;
+  font-size: 24px;
+  width: 100%;
+
+  > span {
+    font-weight: bold;
+    text-align: center;
+  }
+`;
+
+export const HeaderImg = styled.img`
+  max-width: 80%;
+`;
 
 export const Main = styled.main``;
 
-export const Footer = styled.footer``;
+export const Footer = styled.footer`
+  background: rgba(24, 25, 37, 100);
+  width: 100%;
+  color: grey;
+  padding: 20px;
+
+  > span {
+    > a:hover {
+      color: blue;
+    }
+  }
+`;
